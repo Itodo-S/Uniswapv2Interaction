2edaeb256197070e671ab10650577a3531b90e21
Itodo <itodosimonitodo1@gmail.com>
2024-09-17 17:49:52 +0100

    first commit

---
+++ contracts/Interfaces/IUniswapV2.sol
@@ -0,0 +1,30 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.2;
+
+interface IUniswapV2Router {
+    //     function removeLiquidity(
+    //     address tokenA,
+    //     address tokenB,
+    //     uint liquidity,
+    //     uint amountAMin,
+    //     uint amountBMin,
+    //     address to,
+    //     uint deadline
+    // ) external returns (uint amountA, uint amountB);
+
+    function swapExactETHForTokens(uint amountOutMin, address[] calldata path, address to, uint deadline)
+        external
+        payable
+        returns (uint[] memory amounts);
+    
+    function swapExactTokensForETH(
+        uint amountIn,
+        uint amountOutMin,
+        address[] calldata path,
+        address to,
+        uint deadline
+    ) external returns (uint[] memory amounts);
+    
+    function factory() external pure returns (address);
+
+}
---
+++ scripts/interactions.ts
@@ -0,0 +1,46 @@
+import { ethers } from "hardhat";
+const helpers = require("@nomicfoundation/hardhat-network-helpers");
+
+async function main() {
+
+    const ROUTER_ADDRESS = "0x7a250d5630B4cF539739dF2C5dAcb4c659F2488D";
+    const WETH = "0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2";
+    const DAI = "0xFbE6F37d3db3fc939F665cfe21238c11a5447831";
+
+    const TOKEN_HOLDER = "0xf584F8728B874a6a5c7A8d4d387C9aae9172D621";
+  
+    await helpers.impersonateAccount(TOKEN_HOLDER);
+    const impersonatedSigner = await ethers.getSigner(TOKEN_HOLDER);
+
+    const WETH_Contract = await ethers.getContractAt("IERC20", WETH, impersonatedSigner);
+
+    const amountOutMin = ethers.parseUnits("0.001", 18);
+    const deadline = Math.floor(Date.now() / 1000) + (60 * 10);
+  
+    const path = [WETH, DAI];
+
+    const ROUTER = await ethers.getContractAt("IUniswapV2", ROUTER_ADDRESS, impersonatedSigner);
+
+    await WETH_Contract.approve(ROUTER, ethers.parseEther("2") );
+
+    console.log("Starting ETH to DAI swap process...");
+    
+    const swapTx = await ROUTER.swapExactETHForTokens(
+        amountOutMin,
+        path,
+        impersonatedSigner.address,
+        deadline,
+        { value: ethers.parseEther("0.1") }  
+    );
+
+    await swapTx.wait();
+    console.log("Swap Tx:", swapTx);
+    console.log("ETH to DAI swap completed successfully. 🤞");
+}
+
+main().catch((error) => {
+    console.error(error);
+    process.exitCode = 1;
+});
+
+// npm install --save-dev hardhat
